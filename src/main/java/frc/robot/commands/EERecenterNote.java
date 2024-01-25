980784201d5fe9b42999689ac164c155e785eb1f
TobyFlowers <tobiasgflowers@gmail.com>
2024-01-25 16:03:29 -0500

    end condition fix

--- src/main/java/frc/robot/commands/EERecenterNote.java
+++ src/main/java/frc/robot/commands/EERecenterNote.java
@@ -39,6 +39,6 @@ public class EERecenterNote extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return !s_EndEffector.hasNoteShooter();
   }
 }
