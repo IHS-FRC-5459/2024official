32b2e4109139a4eb3820b55807e16199b4974af0
TobyFlowers <tobiasgflowers@gmail.com>
2024-01-26 16:00:11 -0500

    climber cmds

---
+++ src/main/java/frc/robot/commands/ClimberDefault.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Climber;
+
+public class ClimberDefault extends Command {
+  private Climber s_climber; 
+  /** Creates a new ClimberDefault. */
+  public ClimberDefault(Climber clibmer) {
+    s_climber = clibmer;
+    addRequirements(s_climber);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    s_climber.setClimberPower(0);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    s_climber.setClimberPower(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
