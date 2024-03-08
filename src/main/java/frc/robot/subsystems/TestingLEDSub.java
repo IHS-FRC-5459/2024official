5ecee4d62336e72818f2959c264aad4156ef0907
TobyFlowers <tobiasgflowers@gmail.com>
2024-03-08 11:32:30 -0800

    LEDTesting

---
+++ src/main/java/frc/robot/subsystems/TestingLEDSub.java
@@ -0,0 +1,24 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+import frc.robot.Constants;
+//import com.ctre.phoenix6.led.CANdle;
+
+public class TestingLEDSub {
+
+    //static CANdle _candle = new CANdle(Constants.LED.CANdleID, Constants.LED.CANDdleCANbus);
+
+    public TestingLEDSub() {
+    
+    }
+
+    // sets the colors of an LED
+    public static void setLED(int LEDId, int[] RGBVal) {
+
+        //_candle.setLEDs(RGBVal[0], RGBVal[1], RGBVal[2], 0, LEDId, 1);
+    
+    }
+
+}
